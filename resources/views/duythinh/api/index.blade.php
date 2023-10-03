d5bff6f9dac968f785947805bf3ef150493d413b
nogaun <dthinh.n96@gmail.com>
2023-10-03 21:30:21 +0700

    #duythinh Add api method and options

--- resources/views/duythinh/api/index.blade.php
+++ resources/views/duythinh/api/index.blade.php
@@ -5,9 +5,16 @@
     <div class="alert alert-secondary">
         Api Service
     </div>
+    <select name="method" id="method">
+        <option value="get">GET</option>
+        <option value="post">POST</option>
+        <option value="put">PUT</option>
+        <option value="delete">DELETE</option>
+    </select>
     <input name="url" id='url' class="form-control mb-1">
+    <textarea class="form-control" id="options"></textarea>
     <div class="my-3">
-        <button class="btn btn-sm btn-dark" onclick="callApi">
+        <button class="btn btn-sm btn-dark" onclick="callApi()">
             Call Api
         </button>
     </div>
@@ -18,8 +25,16 @@
     <script>
         async function callApi() {
             const url = document.getElementById('url').value
-
-            const response = await fetch(url);
+            const selectElement = document.getElementById('url').value
+            const selectedOption = selectElement.options[selectElement.selectedIndex];
+            const selectedValue = selectedOption.value;
+            const options = document.getElementById('options').innerHTML;
+            const response = await fetch(
+                url,
+                {
+                    method: selectedValue,
+                    options: options,
+                });
 
             document.getElementById('response').innerHTML = JSON.stringify(response)
             console.log(response);
