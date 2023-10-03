885b8ce20bde13bcca63a5987cf77acbe88bd9d3
nogaun <dthinh.n96@gmail.com>
2023-10-03 21:08:29 +0700

    #duythinh fixed feeback

--- routes/personal/duythinh.php
+++ routes/personal/duythinh.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\duythinh\CloudinaryController;
 use App\Http\Controllers\duythinh\GuideController;
+use App\Http\Controllers\duythinh\MainController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('', [MainController::class, 'index']);
 
 Route::prefix('guide')->group(function () {
     Route::get('', [GuideController::class, 'index']);
 });
+
+Route::prefix('cloudinary')->group(function () {
+    Route::get('', [CloudinaryController::class, 'index']);
+    Route::post('upload', [CloudinaryController::class, 'upload']);
+});
+
+Route::prefix('react-component')->group(function () {
+    Route::get('combobox', function () {
+        return view('duythinh.react-component.combobox');
+    });
+});
+
